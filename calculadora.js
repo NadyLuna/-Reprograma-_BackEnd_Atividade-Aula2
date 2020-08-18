a3bb1c513b53909c8a04fcebb65c5d7713c7dd0a
NadyLuna <nadyedja@gmail.com>
2020-08-18 18:01:47 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,2 +1,9 @@
 const input = require("readline-sync")
 
+
+const nota1 = input.question('Qual a nota que você tirou?  )
+const nota2 = input.question('Qual a nota que você tirou?  )
+const nota3 = input.question('Qual a nota que você tirou?  )
+const nota4 = input.question('Qual a nota que você tirou?  )
+const nota5 = input.question('Qual a nota que você tirou?  )
+const nota6 = input.question('Qual a nota que você tirou?  )
